76c7736686a79c38c61003505c64c8765d5dfa79
sekerpare <52707710+sekerpare@users.noreply.github.com>
2023-01-21 18:32:08 +0300

    Add files via upload

---
+++ build/CMakeFiles/face_reconstruction.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/face_reconstruction.dir/main.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/main.cpp.o.d"
+  "face_reconstruction"
+  "face_reconstruction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/face_reconstruction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
