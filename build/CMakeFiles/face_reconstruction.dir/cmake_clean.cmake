6043ba319f76938b819bcb2eda684d0136847e70
azad96 <azad.baykara96@gmail.com>
2023-02-04 15:18:50 +0100

    updates

---
+++ build/CMakeFiles/face_reconstruction.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/face_reconstruction.dir/main.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/main.cpp.o.d"
+  "CMakeFiles/face_reconstruction.dir/src/FreeImageHelper.cpp.o"
+  "CMakeFiles/face_reconstruction.dir/src/FreeImageHelper.cpp.o.d"
+  "face_reconstruction"
+  "face_reconstruction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/face_reconstruction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
